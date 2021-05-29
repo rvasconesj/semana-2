72072dd39451b94928a09e5d8850fa2ce9c0944a
rvasconesj <khristinevwolf@gmail.com>
2021-05-29 10:20:04 -0500

    evaluacion 1 - hasta ejercicio 5

--- reto 1/evaluacion 1.js
+++ reto 1/evaluacion 1.js
@@ -1,12 +1,39 @@
-var name = 'Rodrigo';
-let admin = name;
+// ejercicio 1
 
-console.log(name)
+//var name = 'Rodrigo';
+//let admin = name;
+
+//console.log(name)
 
           //  //alert(admin);
-alert(`Hello ${admin}`);
+//alert(`Hello ${admin}`);
 
 // ejercicio 2
 
-//var planet = 'Tierra';
-//let 
+var planet = 'Tierra';
+var visitWebsite = 'visit' 
+
+// ejercicio 3
+
+//const birthday = '18.04.1982';
+//const age = someCode(birthday);
+
+//¿Sería correcto usar mayúsculas para birthday? para age? ¿O incluso para ambos?
+// Se puede utilizar, sim embargo, se recomienda usar en minúscula con la primera letra del segundo término en mayúscula
+// para que sea más entendible.
+
+// ejercicio 4
+
+let name = "Ilya";
+//alert( `hello ${1}` ); // carga el alert con el  'hello 1'
+//alert( `hello ${"name"}` ); //  el alert con ' hello name'
+//alert( `hello ${name}` ); // se genera el alert en el navegador hello 'Ilya'
+
+// ejercicio 5
+
+var user = {
+    surName: 'John',
+    lastName: 'Smith'
+}
+
+console.log(user)
