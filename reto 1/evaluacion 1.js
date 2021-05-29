4ae5db4f5a88c94b67036fcabbb6657c9a9e9768
Daniel Soto <dasg.1993@gmail.com>
2021-05-29 13:18:45 -0500

    Update evaluacion 1.js

--- reto 1/evaluacion 1.js
+++ reto 1/evaluacion 1.js
@@ -41,11 +41,22 @@ let name = "Ilya";
 //const originalString = 'John'  
 //console.log('newString', originalString.replace('John', 'Pete'))
 
-const user2 = {
-    lastName: 'Smith'
-}
+let user = {};
 
-console.log(user2)
+user.name = 'John';
+user.lastname = 'Smith';
+
+user.name = 'Pete';
+
+delete user.lastname;
+
+console.log('user', user);
+
+// const user2 = {
+//     lastName: 'Smith'
+// }
+
+// console.log(user2)
 
 
 // ejercicio 6
