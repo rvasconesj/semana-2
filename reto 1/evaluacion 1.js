c750fbee2d9ebeec34e0192778f29cb87d7de090
corsa <84633684+rvasconesj@users.noreply.github.com>
2021-05-30 15:42:04 -0500

    evaluación 1 completo

--- reto 1/evaluacion 1.js
+++ reto 1/evaluacion 1.js
@@ -89,5 +89,23 @@ let fullSalaries = 0;
 
 // ejercicio 8
 
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My Menu"
+};
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if(typeof obj[key] === 'number') {
+         obj[key] *= 2;
+        }
+    }
+    return obj;
+}
+
+console.log(menu);
+console.log(multiplyNumeric(menu));
+
 
 
