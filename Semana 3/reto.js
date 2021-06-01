986aa3829932f43e3644b59d57fb0d07ff5e058d
Daniel Soto <dasg.1993@gmail.com>
2021-05-31 21:59:29 -0500

    identation

--- Semana 3/reto.js
+++ Semana 3/reto.js
@@ -1,11 +1,10 @@
 
 for (let i = 0; i < 20; i++) {
-     if(i % 2 == 0) {
-    console.log(i);
-    console.log("Es par");
+    if(i % 2 == 0) {
+        console.log(i);
+        console.log("Es par");
+    } else {
+        console.log(i);
+        console.log("Es impar");
+    }
 }
-
- else {
-    console.log(i);
-    console.log("Es impar");
- }}
