90a4be54d6d44b588d13362213e6b0cd07e1d4e0
rvasconesj <khristinevwolf@gmail.com>
2021-05-31 22:08:57 -0500

    reto - ejercicio 2

--- Semana 3/reto.js
+++ Semana 3/reto.js
@@ -1,11 +1,29 @@
 
 for (let i = 0; i < 20; i++) {
-     if(i % 2 == 0) {
-    console.log(i);
-    console.log("Es par");
+    if(i % 2 == 0) {
+        console.log(i);
+        console.log("Es par");
+    } else {
+        console.log(i);
+        console.log("Es impar");
+    }
 }
 
- else {
-    console.log(i);
-    console.log("Es impar");
- }}
+
+ //ejercicio 2
+
+ for (let i = 0; i < 10; i++) {
+    if(i % 2 == 0) {
+   console.log(i);
+   console.log("Es par");
+} else {
+   console.log(i);
+   console.log("Es impar");
+}}
+
+//ejercicio 2
+
+for (let i = 0; i < 11; i++) {
+    console.log('i * 2', i * 2 )
+
+}
